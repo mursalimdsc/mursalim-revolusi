6ef7a9197f0693d3c9c382101c6c89c307c1db66
Mursalim <Salimbyan.nugroho@gmail.com>
2022-11-07 10:10:56 +0700

    Menambahkan latihan 2 pyhton

---
+++ latihan2.py
@@ -0,0 +1,10 @@
+# Latihan 2
+
+# Revolusi 2023
+'''
+  - Mendapatkan beasiswa S3 di dalam maupun di Luar Negeri
+  - Melaksanakan riset tentang Data Science, Machine Learning dan Computer Vision
+  - Mengurus NIDN
+
+'''
+
