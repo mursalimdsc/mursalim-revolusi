c3f6b2f2a51911c43d2b7562109cc64279b306a6
mursalimdsc <117145822+mursalimdsc@users.noreply.github.com>
2022-11-07 07:42:06 +0700

    Menambahkan file latiahn 2 colab

---
+++ latihan2_kolab.py
@@ -0,0 +1,13 @@
+# Fungsi
+
+def perkalian(a,b):
+  nilai_a = a
+  nilai_b = b
+  total = nilai_a * nilai_b
+  return total
+
+def penjumlahan(a,b):
+  nilai_a = a
+  nilai_b = b
+  total = nilai_a + nilai b
+  return total
