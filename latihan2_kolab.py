69d1f80a66eee58e86056ad3a1fce400b8768e2a
mursalimdsc <117145822+mursalimdsc@users.noreply.github.com>
2022-11-07 07:59:42 +0700

    Menambah Eksekusi fungsi penjumlahan

--- latihan2_kolab.py
+++ latihan2_kolab.py
@@ -1,5 +1,4 @@
 # Fungsi
-
 def perkalian(a,b):
   nilai_a = a
   nilai_b = b
@@ -11,3 +10,10 @@ def penjumlahan(a,b):
   nilai_b = b
   total = nilai_a + nilai b
   return total
+
+
+# Eksekusi fungsi
+
+nilai_a = int(input("Masukkan nilai A :"))
+nilai_b = int(input("Masukkan nilai B :"))
+print(penjumlahan(nilai_a,nilai_b))
